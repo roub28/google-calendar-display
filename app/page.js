e72e6b2e629e928d88eb894adaf1e20f4a1399e3
roub28 <roub_28@hotmail.fr>
2023-10-04 10:11:21 +0200

    Automaticaly display attachment and tailwind functionnal

--- app/page.js
+++ app/page.js
@@ -1,17 +1,26 @@
 import { getGcalData } from 'components/getGcalData'
+import Image from 'next/image'
 
 export default async function Page() {
   const data = await getGcalData()
   const summaryElements = data.items.map((item) => (
-    <div key={item.id}>
+    <div class="basis-1/4" key={item.id}>
       Summary: {item.summary}
+      {item.attachments ? (
+          <Image
+          src={`https://drive.google.com/uc?export=view&id=${item.attachments[0].fileUrl.split(/[=&]/)[1]}`}
+          width={200}
+          height={200}
+          alt="Picture of the author"
+        />
+      ) : null}
     </div>
   ));
   
   return (
     <div>
-      <h1>Hello, Next.js!</h1>
-      <div>
+      <h1 className="text-sm ...">Events to come</h1>
+      <div className="flex flex-row">
         {summaryElements}
       </div>
     </div>
